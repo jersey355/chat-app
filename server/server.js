cb094eb64e4b7a35c060c20600345b95f1252760
jersey355 <ed.valdez@live.com>
2018-11-30 17:27:22 -0500

    Added websocket with connect and disconnect handlers on both client and server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 // allows you to serve static content
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+    console.log('New client connected!');
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected!');
+    });
+});
+
 // start server
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Chat server listening on port ${port} ...`);
 });
